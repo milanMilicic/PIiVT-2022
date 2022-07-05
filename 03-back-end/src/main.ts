64873b566a5b929b62c9c66f80b1dfcde921d535
Milan Michael Miličić <milanm.milicic@gmail.com>
2022-07-05 11:12:01 +0200

    Set up an activity logger

--- 03-back-end/src/main.ts
+++ 03-back-end/src/main.ts
@@ -1,1 +1,47 @@
-console.log('Test');
+import * as express from "express";
+import * as cors from "cors";
+import IConfig from "./common/IConfig.interface";
+import DevConfig from "./configs";
+import CategoryController from "./components/category/CategoryController.controller";
+import CategoryService from "./components/category/CategoryService.service";
+import * as fs from "fs";
+import * as morgan from "morgan";
+
+
+const config: IConfig = DevConfig;
+
+fs.mkdirSync("./logs", {
+    mode: 0o755,
+    recursive: true,
+});
+
+const application: express.Application = express();
+
+application.use(morgan(":date[iso]\t:remote-addr\t:method\t:url\t:status\t:res[content-length] bytes\t:response-time ms", {
+    stream: fs.createWriteStream("./logs/access.log")
+}));
+
+application.use(cors());
+application.use(express.json());
+
+application.use(config.server.static.route, express.static(config.server.static.path, {
+    index: config.server.static.index,
+    dotfiles: config.server.static.dotfiles,
+    cacheControl: config.server.static.cacheControl,
+    etag: config.server.static.etag,
+    maxAge: config.server.static.maxAge,
+}));
+
+
+const categoryService: CategoryService = new CategoryService();
+const categoryController: CategoryController = new CategoryController(categoryService);
+
+application.get('/api/categories', categoryController.getAll.bind(categoryController));
+application.get('/api/categories/:cid', categoryController.getById.bind(categoryController));
+
+application.use( (req, res) => {
+    res.sendStatus(404);
+});
+
+application.listen(config.server.port);
+
