ad0dd19c45725418dfbf7bb32735875e72985315
Milan Michael Miličić <milanm.milicic@gmail.com>
2022-07-05 18:04:49 +0200

    Add ajv validator for category when adding

--- 03-back-end/src/components/category/dto/IAddCategory.dto.ts
+++ 03-back-end/src/components/category/dto/IAddCategory.dto.ts
@@ -1,4 +1,33 @@
-export default interface IAddCategory {
+import Ajv from "ajv";
+import IServiceData from "../../../common/IServiceData.interface";
+
+const ajv = new Ajv();
+
+
+export default interface IAddCategory extends IServiceData {
     name: string;
     hourlyPrice: number;
 }
+
+
+const AddCategoryValidator = ajv.compile({
+    type: "object",
+    properties: {
+        name: {
+            type: "string",
+            minLength: 3,
+            maxLength: 32,
+        },
+        hourlyPrice: {
+            type: "integer",
+            minimum: 300, 
+        }
+    },
+    required: [
+        "name",
+        "hourlyPrice"
+    ],
+    additionalProperties: false,
+});
+
+export { AddCategoryValidator };
