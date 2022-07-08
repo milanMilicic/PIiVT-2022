b050c85ee391d6d2c162bd8a81babd0667a9d53b
Milan Michael Miličić <milanm.milicic@gmail.com>
2022-07-08 11:47:14 +0200

    Implement method get by month for calculation

--- 03-back-end/src/components/calculation/CalculationController.controller.ts
+++ 03-back-end/src/components/calculation/CalculationController.controller.ts
@@ -32,5 +32,21 @@ export default class CalculationController extends BaseController {
             res.status(500).send(error?.message);
         });
     }
-    
+
+    async getByMonth(req: Request, res: Response){
+        const monthId: number = +req.params.mid;
+
+        this.services.calculation.getByMonthId(monthId)
+        .then(result => {
+            if(result.length === 0){
+                return res.status(404).send(`Calculation for month ${monthId} not found`);
+            }
+
+            res.send(result);
+        })
+        .catch(error => {
+            res.status(500).send(error?.message);
+        });
+    }
+
 }
