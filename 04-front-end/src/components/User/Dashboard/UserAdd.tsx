0d02359fb599e11f2d9318c8dcb86e3cfac2f9cc
Milan Michael Miličić <milanm.milicic@gmail.com>
2022-09-19 11:33:42 +0200

    Implement show/hide password

--- 04-front-end/src/components/User/Dashboard/UserAdd.tsx
+++ 04-front-end/src/components/User/Dashboard/UserAdd.tsx
@@ -9,6 +9,10 @@ export default function UserAdd(){
     const [passwordAgain, setPasswordAgain] = useState<string>("");
     const [message, setMessage] = useState<string>("");
 
+
+    
+
+
     const addNewUser = () => {
         var re = new RegExp("^[a-z0-9\-]{5,32}$");
         let paragraf = document.getElementById('message');
@@ -54,6 +58,57 @@ export default function UserAdd(){
             }, 2000);
         });
     }
+
+
+
+    let usernameInput = document.getElementById('username');
+    usernameInput?.addEventListener('keyup', function(){
+        let div = document.getElementById('username-info');
+        if(div){
+            div.style.visibility = "visible";
+        }
+        
+        return;
+        
+    });
+
+
+    let passwordInput = document.getElementById('password');
+    passwordInput?.addEventListener('keyup', function(){
+        let div = document.getElementById('password-info');
+        if(div){
+            div.style.visibility = "visible";
+        }
+        
+        return;
+        
+    });
+
+
+    
+    var state1 = false;
+    function toggle1(){
+        if(state1){
+            document.getElementById('password1')?.setAttribute('type', 'password');
+            state1 = false;
+        } else {
+            document.getElementById('password1')?.setAttribute('type', 'text');
+            state1 = true;
+        }
+    }
+
+    var  state2 = false;
+    function toggle2(){
+        if(state2){
+            document.getElementById('password2')?.setAttribute('type', 'password');
+            state2 = false;
+        } else {
+            document.getElementById('password2')?.setAttribute('type', 'text');
+            state2 = true;
+        }
+    }
+    
+    
     
 
     return (
@@ -62,21 +117,21 @@ export default function UserAdd(){
                 <h1 className="h3 mb-3">Add new user</h1>
                 <div className="form-group mb-1">
                     <div className="input-group">
-                        <input className="form-control" type="text" placeholder="Enter your username" value={username} onChange={e => setUsername(e.target.value)} />
+                        <input id="username" className="form-control" type="text" placeholder="Enter your username" value={username} onChange={e => setUsername(e.target.value)} />
                     </div>
-                    <div id="username" className="paragraphs">Username must be between 5 and 32 characters and can have lower case letters, <br /> numbers and character '-'</div>
+                    <div id="username-info" className="paragraphs">Username must be between 5 and 32 characters and can have lower case letters, <br /> numbers and character '-'</div>
                 </div>
                 <div className="form-group mb-1">
                     <div className="input-group">
-                        <input className="form-control" type="password" placeholder="Enter your password" value={password} onChange={e => setPassword(e.target.value)}/>
-                        <div title="Show password" className="btn btn-warning">Show</div>
+                        <input id="password1" className="form-control" type="password" placeholder="Enter your password" value={password} onChange={e => setPassword(e.target.value)}/>
+                        <div title="Show password" className="btn btn-warning" onClick={() => toggle1()}>Show</div>
                     </div>
-                    <div id="password" className="paragraphs">Password must be at least 6 characters, contain at least one upper case letter and a number</div>
+                    <div id="password-info" className="paragraphs">Password must be at least 6 characters, contain at least one upper case letter and a number</div>
                 </div>
                 <div className="form-group mb-3">
                     <div className="input-group">
-                        <input className="form-control" type="password" placeholder="Enter your password again" value={passwordAgain} onChange={e => setPasswordAgain(e.target.value)}/>
-                        <div title="Show password" className="btn btn-warning">Show</div>
+                        <input id="password2" className="form-control" type="password" placeholder="Enter your password again" value={passwordAgain} onChange={e => setPasswordAgain(e.target.value)}/>
+                        <div title="Show password" className="btn btn-warning" onClick={() => toggle2()}>Show</div>
                     </div>
                 </div>
                 {(password !== passwordAgain) && (passwordAgain !== "") && (password !== "") && <p className="alert alert-warning mb-3">Passwords do not match!</p>}
